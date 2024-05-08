2a0784c35f18c2133f816b361d537e174369adb4
avay12 <avaygc@gmail.com>
2024-05-08 12:28:14 +0545

    inital commit

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import Signup from "./page/Signup";
+import Company from "./page/company";
+
+function App() {
+  return (
+    <div>
+      <Company />
+      <Signup />
+    </div>
+  );
+}
+
+export default App;
